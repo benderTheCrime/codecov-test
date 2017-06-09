e02ff9fedbb1de34c3f79855676a4278aca97086
Joe Groseclose <joe.groseclose@adp.com>
2017-06-09 09:14:12 -0400

    add test for base case

--- test/index.spec.js
+++ test/index.spec.js
@@ -4,6 +4,9 @@ const { bar, foo } = require('../');
 
 describe('index', function() {
     describe('bar', function() {
+        it('should return 1 given 1', function() {
+            expect(foo(1)).to.equal(1);
+        });
         it('should return 1 given 2', function() {
             expect(foo(2)).to.equal(1);
         });
