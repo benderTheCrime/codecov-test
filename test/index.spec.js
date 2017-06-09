31e39278d0c2119a7ca1621df647050764831f43
Joe Groseclose <joe.groseclose@adp.com>
2017-06-09 09:22:20 -0400

    add coverage

--- test/index.spec.js
+++ test/index.spec.js
@@ -5,15 +5,15 @@ const { bar, foo } = require('../');
 describe('index', function() {
     describe('bar', function() {
         it('should return 1 given 1', function() {
-            expect(foo(1)).to.equal(1);
+            expect(bar(1)).to.equal(1);
         });
         it('should return 1 given 2', function() {
-            expect(foo(2)).to.equal(1);
+            expect(bar(2)).to.equal(1);
         });
     });
     describe('foo', function() {
         it('should return 1', function() {
-            expect(foo()).to.equal(1);
+            expect(foo(1)).to.equal(1);
         });
     });
 });
